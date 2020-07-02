0c230820b4c58c5e046d0e4ab833365cdf7218c1
Arseniy Lyubogashchinskiy <tarsenicum@mail.ru>
2020-07-02 18:11:11 +0800

    1

--- soundofsilence/Form1.Designer.cs
+++ soundofsilence/Form1.Designer.cs
@@ -36,6 +36,13 @@
             this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton4 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton5 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton6 = new System.Windows.Forms.ToolStripButton();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.volumeMeter1 = new NAudio.Gui.VolumeMeter();
             this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -44,7 +51,11 @@
             this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripButton1,
             this.toolStripButton2,
-            this.toolStripSplitButton1});
+            this.toolStripButton6,
+            this.toolStripSplitButton1,
+            this.toolStripButton3,
+            this.toolStripButton4,
+            this.toolStripButton5});
             this.toolStrip1.Location = new System.Drawing.Point(0, 0);
             this.toolStrip1.Name = "toolStrip1";
             this.toolStrip1.Size = new System.Drawing.Size(800, 25);
@@ -58,7 +69,7 @@
             this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.toolStripButton1.Name = "toolStripButton1";
             this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
-            this.toolStripButton1.Text = "toolStripButton1";
+            this.toolStripButton1.Text = "Start recording";
             this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
             // 
             // toolStripButton2
@@ -68,7 +79,7 @@
             this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.toolStripButton2.Name = "toolStripButton2";
             this.toolStripButton2.Size = new System.Drawing.Size(23, 22);
-            this.toolStripButton2.Text = "toolStripButton2";
+            this.toolStripButton2.Text = "Stop recording";
             this.toolStripButton2.Click += new System.EventHandler(this.toolStripButton2_Click);
             // 
             // toolStripSplitButton1
@@ -106,11 +117,84 @@
             this.textBox1.Text = "C:/demo.wav";
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(136, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Stopped";
+            // 
+            // toolStripButton3
+            // 
+            this.toolStripButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton3.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton3.Image")));
+            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton3.Name = "toolStripButton3";
+            this.toolStripButton3.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton3.Text = "Start playback";
+            this.toolStripButton3.Click += new System.EventHandler(this.toolStripButton3_Click);
+            // 
+            // toolStripButton4
+            // 
+            this.toolStripButton4.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton4.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton4.Image")));
+            this.toolStripButton4.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton4.Name = "toolStripButton4";
+            this.toolStripButton4.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton4.Text = "Stop playback";
+            this.toolStripButton4.Click += new System.EventHandler(this.toolStripButton4_Click);
+            // 
+            // toolStripButton5
+            // 
+            this.toolStripButton5.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton5.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton5.Image")));
+            this.toolStripButton5.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton5.Name = "toolStripButton5";
+            this.toolStripButton5.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton5.Text = "Pause playback";
+            this.toolStripButton5.Click += new System.EventHandler(this.toolStripButton5_Click);
+            // 
+            // toolStripButton6
+            // 
+            this.toolStripButton6.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton6.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton6.Image")));
+            this.toolStripButton6.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton6.Name = "toolStripButton6";
+            this.toolStripButton6.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton6.Text = "Bytes to Numbers";
+            this.toolStripButton6.Click += new System.EventHandler(this.toolStripButton6_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(226, 28);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "C:/demo2.wav";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // volumeMeter1
+            // 
+            this.volumeMeter1.Amplitude = 0F;
+            this.volumeMeter1.Location = new System.Drawing.Point(12, 90);
+            this.volumeMeter1.MaxDb = 18F;
+            this.volumeMeter1.MinDb = -60F;
+            this.volumeMeter1.Name = "volumeMeter1";
+            this.volumeMeter1.Size = new System.Drawing.Size(75, 23);
+            this.volumeMeter1.TabIndex = 4;
+            this.volumeMeter1.Text = "volumeMeter1";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.volumeMeter1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.toolStrip1);
             this.Name = "Form1";
@@ -131,6 +215,13 @@
         private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
         private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolStripButton toolStripButton3;
+        private System.Windows.Forms.ToolStripButton toolStripButton4;
+        private System.Windows.Forms.ToolStripButton toolStripButton5;
+        private System.Windows.Forms.ToolStripButton toolStripButton6;
+        private System.Windows.Forms.TextBox textBox2;
+        private NAudio.Gui.VolumeMeter volumeMeter1;
     }
 }
 
--- soundofsilence/Form1.cs
+++ soundofsilence/Form1.cs
@@ -11,6 +11,7 @@ using NAudio.Wave;
 using NAudio.FileFormats;
 using NAudio.CoreAudioApi;
 using NAudio;
+using System.IO;
 
 namespace soundofsilence
 {
@@ -20,10 +21,20 @@ namespace soundofsilence
         WaveIn waveIn;
         //Class for record in file
         WaveFileWriter writer;
-        //file
+        //files
         string outputFileName = "C:/demo.wav";
+        string inputFileName = "C:/demo2.wav";
         //device number
         int deviceNumber = 0;
+        //output device
+        WaveOutEvent outputDevice;
+        //output stream
+        AudioFileReader audioFile;
+        //recording flag
+        bool isRecording;
+        //array of max samples
+        float[] maxsamples;
+
 
         public Form1()
         {
@@ -39,13 +50,40 @@ namespace soundofsilence
             }
             else
             {
-                writer.Write(e.Buffer, 0, e.BytesRecorded);
+                if (isRecording)
+                {
+                    writer.Write(e.Buffer, 0, e.BytesRecorded);
+                }
+                
+
+                float max = 0;
+                //interpret as 16 bit audio
+                maxsamples = new float[e.BytesRecorded];
+                for (int index = 0; index< e.BytesRecorded; index += 2)
+                {
+                    short sample = (short)((e.Buffer[index + 1] << 8) | e.Buffer[index + 0]);
+                    //to floating point
+                    var sample32 = sample / 32768f;
+                    //absolute value
+                    if (sample32 < 0) sample32 = -sample32;
+                    //is this the max value?
+                    if (sample32 > max) max = sample32;
+                    volumeMeter1.Amplitude = 100 * max;
+                    maxsamples[index] = max;
+                }
+                var byteArray = new byte[maxsamples.Length * 4];
+                Buffer.BlockCopy(maxsamples, 0, byteArray, 0, byteArray.Length);
+                using (FileStream fstream = new FileStream("C:/demo.txt", FileMode.Open))
+                {
+                    fstream.Write(byteArray, 0, byteArray.Length);
+                }
             }
         }
 
         void StopRecording()
-        {            
-            MessageBox.Show("Stop Recording");
+        {
+            label1.BackColor = Color.Red;
+            label1.Text = "Stopped";
             waveIn.StopRecording();
         }
 
@@ -57,6 +95,7 @@ namespace soundofsilence
             }
             else
             {
+                isRecording = false;
                 waveIn.Dispose();
                 waveIn = null;
                 writer.Close();
@@ -64,11 +103,22 @@ namespace soundofsilence
             }
         }
 
+        private void OnPlayBackStopped(object sender, StoppedEventArgs e)
+        {
+            outputDevice.Dispose();
+            outputDevice = null;
+            audioFile.Dispose();
+            audioFile = null;
+        }
+
         private void toolStripButton1_Click(object sender, EventArgs e)
         {
+            
             try
             {
-                MessageBox.Show("Start Recording");
+                isRecording = true;
+                label1.BackColor = Color.Lime;
+                label1.Text = "Recording";
                 waveIn = new WaveIn();
                 //Дефолтное устройство для записи (если оно имеется)
                 //встроенный микрофон ноутбука имеет номер 0
@@ -78,7 +128,7 @@ namespace soundofsilence
                 //Прикрепляем обработчик завершения записи
                 waveIn.RecordingStopped += waveIn_RecordingStopped;
                 //Формат wav-файла - принимает параметры - частоту дискретизации и количество каналов(здесь mono)
-                waveIn.WaveFormat = new WaveFormat(8000, 1);
+                waveIn.WaveFormat = new WaveFormat(44100, 1);
                 //Инициализируем объект WaveFileWriter
                 writer = new WaveFileWriter(outputFileName, waveIn.WaveFormat);
                 //Начало записи
@@ -111,6 +161,41 @@ namespace soundofsilence
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
             outputFileName = textBox1.Text;
+        }
+
+        private void toolStripButton3_Click(object sender, EventArgs e)
+        {
+            if (outputDevice == null)
+            {
+                outputDevice = new WaveOutEvent();
+                outputDevice.PlaybackStopped += OnPlayBackStopped;
+            }
+            if (audioFile == null)
+            {
+                audioFile = new AudioFileReader(inputFileName);
+                outputDevice.Init(audioFile);
+            }
+            outputDevice.Play();
+        }
+
+        private void toolStripButton4_Click(object sender, EventArgs e)
+        {
+            outputDevice?.Stop();
+        }
+
+        private void toolStripButton5_Click(object sender, EventArgs e)
+        {
+            outputDevice?.Pause();
+        }
+
+        private void toolStripButton6_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            inputFileName = textBox2.Text;
         }
     }
 }
